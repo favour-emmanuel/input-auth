41e293b1ae0e5f41bb96778a1d839089054f7f60
Favour Emmanuel <iyenefavour@gmail.com>
2024-10-13 01:48:27 +0100

    added a schema validation for formdata

--- src/component/Signup.tsx
+++ src/component/Signup.tsx
@@ -1,7 +1,42 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import atmImage from "../assets/Group11.png";
+import { z, ZodType } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type formData = {
+  email: string;
+  date: string;
+  password: string;
+};
 
 const SignUp = () => {
+  // scheme input
+  const schema: ZodType<formData> = z.object({
+    email: z.string().email({ message: "This is not a valid email" }),
+    date: z.string().refine(
+      (val) => {
+        const date = new Date(val);
+        return date >= new Date("1950-01-01") && date <= new Date("1999-01-01");
+      },
+      { message: "Date must be between 1950 and 1999." }
+    ),
+    password: z
+      .string()
+      .min(5, { message: "Your Password is too short" })
+      .max(10, { message: "Your password is too long" }),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formData>({ resolver: zodResolver(schema) });
+
+  const formSubmit = (data: formData) => {
+    console.log(data);
+  };
+
   return (
     <div className="py-10 w-full text-white ">
       <div className="my-5 w-full flex justify-center flex-col items-center">
@@ -9,7 +44,10 @@ const SignUp = () => {
           Go back to Home
           <Icon icon="ic:baseline-home" className="text-lg" />
         </h1>
-        <div className="my-5 flex justify-center w-full max-w-[54rem] shadow-lg shadow-appCyan/15">
+        <form
+          onSubmit={handleSubmit(formSubmit)}
+          className="my-5 flex justify-center w-full max-w-[54rem] shadow-lg shadow-appCyan/15"
+        >
           <div className="bg-appfadeGray px-5 py-3 w-full">
             <h2 className="font-bold text-2xl text-white mb-5">
               Sign in to OnePoint
@@ -22,10 +60,16 @@ const SignUp = () => {
                 </label>
                 <div className="my-3 w-full">
                   <input
-                    type="email"
+                    type="text"
                     placeholder="youremail@gmail.com"
+                    {...register("email")}
                     className="py-3 px-4 w-full max-w-[23rem] rounded-md outline-slate-700 bg-appfadeGray/70 border border-appCyan/70"
                   />
+                  {errors.email && (
+                    <h3 className="text-[#f5314b] text-sm">
+                      {errors.email.message}
+                    </h3>
+                  )}
                 </div>
                 <label htmlFor="" className="my-2">
                   Enter Date
@@ -34,8 +78,14 @@ const SignUp = () => {
                   <input
                     type="date"
                     placeholder="Enter date of birth"
+                    {...register("date")}
                     className="py-3 px-4 w-full max-w-[23rem] rounded-md outline-slate-700 bg-appfadeGray/70 border border-appCyan/70"
                   />
+                  {errors.date && (
+                    <span className="text-[#f5314b] text-sm">
+                      {errors.date.message}
+                    </span>
+                  )}
                 </div>
 
                 <label htmlFor="" className="my-2">
@@ -45,8 +95,14 @@ const SignUp = () => {
                   <input
                     type="password"
                     placeholder="set password"
+                    {...register("password")}
                     className="py-3 px-4 w-full max-w-[23rem] rounded-md outline-slate-700 bg-appfadeGray/70 border border-appCyan/70"
                   />
+                  {errors.password && (
+                    <span className="text-[#f5314b] text-sm">
+                      {errors.password.message}
+                    </span>
+                  )}
                 </div>
               </div>
             </div>
@@ -68,7 +124,7 @@ const SignUp = () => {
           <div className="bg-appCyan/80 py-3 px-4 w-full max-w-[27rem] flex items-center">
             <img src={atmImage} alt="/" className="w-[500px]" />
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
